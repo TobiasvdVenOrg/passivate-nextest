0d9dbb72d7de5863c115220cef03d0314e313054
TobiasvdVen <tobiasvdven@hotmail.com>
2026-02-12 19:31:07 +0100

    Flake.nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "nextest-passivate";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-25.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        libPath = with pkgs; lib.makeLibraryPath [
+          wayland
+          libxkbcommon
+          libGL
+        ];
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            (
+              rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+                extensions = [ "rust-src" "rust-analyzer" ];
+                #targets = [ "x86_64-unknown-linux-gnu" ]
+              })
+            )
+          ];
+
+          LD_LIBRARY_PATH = libPath;
+        };
+      }
+    );
+}
